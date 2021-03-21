1830ca4220756eb8cabefa8a09987f853331e62a
Adamets Vladislav <v.adamets@consult-info.ru>
2021-03-21 16:19:35 +0200

    feet: some refactoring

--- Popup.js
+++ Popup.js
@@ -1,5 +1,9 @@
 ;(function (window) {
     class Popup {
+        #state = {
+            isAjax: false,
+        };
+
         constructor(params) {
             if (params instanceof Object) {
                 this.options = Object.assign({}, Popup.defaultParams, params);
@@ -40,10 +44,14 @@
 
             if (content !== null && typeof content === "string") {
                 if (content.indexOf("/") >= 0 || ajax === true) {
-                    this.$promise = fetch(content).then(
-                        (value) => (value.ok ? value.text() : "404 Not found"),
-                        (error) => "Check your internet connection"
-                    );
+                    this.#state.isAjax = true;
+
+                    this.$promise = (noFetch = false) => {
+                        return noFetch ? Promise.resolve() : fetch(content).then(
+                            (value) => (value.ok ? value.text() : "404 Not found"),
+                            (error) => "Check your internet connection"
+                        );
+                    }
                 } else {
                     this.$promise = new Promise((resolve, reject) => {
                         let popupElement = document.querySelector(content);
@@ -56,7 +64,7 @@
                     });
                 }
             } else if (content instanceof Node) {
-                this.$promise = Promise.resolve(content.innerHTML)
+                this.$promise = Promise.resolve(content.innerHTML);
             } else {
                 this.$promise = Promise.reject('Content Type Not Supported');
             }
@@ -64,50 +72,60 @@
 
         addTitle() {
             let $contentWrap = this.$wrap.querySelector(".popup__content-wrap");
+            let $popupWrap = this.$wrap.querySelector(".popup__wrap");
 
             this.$contentWrap = $contentWrap;
 
             if (this.options.title === false || !this.options.title) {
-                $contentWrap.removeChild($contentWrap.querySelector(".popup__title"));
+                $popupWrap.removeChild(this.$wrap.querySelector(".popup__title"));
             } else {
-                $contentWrap.querySelector(".popup__title").innerHTML = this.options.title;
+                this.$wrap.querySelector(".popup__title").innerHTML = this.options.title;
             }
         }
 
         open() {
-            if (this.$promise !== undefined && this.$promise?.then !== undefined) {
-                this.$promise.then(
-                    (result) => {
-                        this.$contentWrap.insertAdjacentHTML("beforeend", result);
+            function showPopup() {
+                !this.isClosing && this.$wrap.classList.add("popup_open");
 
-                        document.body.appendChild(this.$wrap);
+                if (window['getScrollBarWidth']) {
+                    Popup.setPadding(getScrollBarWidth() + "px", this.options.paddingRightElements);
+                }
 
-                        if (typeof this.options.onAfterAppend === "function") {
-                            this.options.onAfterAppend(this.$wrap);
-                        }
-                    },
-                    (error) => {
-                        this.$contentWrap.insertAdjacentHTML("afterbegin", "Something went wrong");
-                        console.log(error);
+                document.addEventListener("keydown", this.handlers.escape);
+
+                if (typeof this.options.onAfterOpen === "function") {
+                    this.options.onAfterOpen.bind(this)(this.$contentWrap.querySelector('.popup__content'));
+                }
+            }
+
+            if (this.$promise !== undefined && this.$promise?.then !== undefined || this.$promise instanceof Function) {
+                const openThen = [(result) => {
+                    this.$contentWrap.querySelector('.popup__content').insertAdjacentHTML("beforeend", result);
+
+                    document.body.appendChild(this.$wrap);
+
+                    showPopup.bind(this)();
+
+                    if (typeof this.options.onAfterAppend === "function") {
+                        this.options.onAfterAppend.bind(this)(this.$contentWrap.querySelector('.popup__content'));
                     }
-                );
+                },
+                    (error) => {
+                        this.$contentWrap.querySelector('.popup__content').insertAdjacentHTML("afterbegin", "Something went wrong");
+                        console.log(error);
+                    }]
+
+                if (this.#state.isAjax) {
+                    this.$promise().then(
+                        ...openThen
+                    )
+                } else
+                    this.$promise.then(
+                        ...openThen
+                    );
             }
 
-            if (this.options.cache) {
-                this.$promise = undefined;
-            }
-
-            !this.isClosing && this.$wrap.classList.add("popup_open");
-
-            if (window['getScrollBarWidth']) {
-                Popup.setPadding(getScrollBarWidth() + "px", this.options.paddingRightElements);
-            }
-
-            document.addEventListener("keydown", this.handlers.escape);
-
-            if (typeof this.options.onAfterOpen === "function") {
-                this.options.onAfterOpen(this.$wrap);
-            }
+            return Promise.resolve();
         }
 
         close() {
@@ -122,11 +140,29 @@
                 document.removeEventListener("keydown", this.handlers.escape);
                 this.isClosing = false;
 
+                const closeThen = [() => {
+                    this.$contentWrap.querySelector('.popup__content').innerHTML = '';
+
+                    if (typeof this.options.onAfterClose === "function") {
+                        this.options.onAfterClose.bind(this)();
+                    }
+                },
+                    (error) => {
+                        this.$contentWrap.querySelector('.popup__content').insertAdjacentHTML("afterbegin", "Something went wrong");
+                        console.log(error);
+                    }]
+
+                if (this.$promise !== undefined && this.$promise?.then !== undefined || this.$promise instanceof Function) {
+                    if (this.#state.isAjax) {
+                        this.$promise(true).then(...closeThen)
+                    } else
+                        this.$promise.then(
+                            ...closeThen
+                        );
+                }
             }, Popup.config.ANIMATION_SPEED);
 
-            if (typeof this.options.onAfterClose === "function") {
-                this.options.onAfterClose(wrap);
-            }
+            return Promise.resolve();
         }
 
         static createWrap() {
@@ -136,9 +172,15 @@
 
             wrap.innerHTML = `
                 <div class="popup__wrap">
-                    <svg class="popup__logo-fixed" width="64" height="16" viewBox="0 0 128 32" fill="none" xmlns="http://www.w3.org/2000/svg"><g fill="#FF005C"><path fill-rule="evenodd" clip-rule="evenodd" d="M119 18h-13v10h13a5 5 0 000-10zm-17-4v18h17a9 9 0 009-9 9 9 0 00-9-9h-17z"/><path d="M126 0h-24v32h4V4h20V0z"/></g><g fill="#FF005C"><path d="M94 0v32h-4V18H70v14h-4V0h4v14h20V0h4z"/><path fill-rule="evenodd" clip-rule="evenodd" d="M44 28c6.627 0 12-5.373 12-12S50.627 4 44 4 32 9.373 32 16s5.373 12 12 12zm0 4c8.837 0 16-7.163 16-16S52.837 0 44 0 28 7.163 28 16s7.163 16 16 16z"/><path d="M22 0L8 14H4V0H0v32h4V18h4l14 14 2.82-2.82L11.64 16 24.82 2.82 22 0z"/></g></svg>
-                      <div class="popup__close" data-close="true"><span class="popup__close_1"></span><span class="popup__close_2"></span></div>
-                    <div class="popup__content-wrap"><h3 class="popup__title"></h3></div>
+                    <h3 class="popup__title"></h3>
+                    <div class="popup__content-wrap">
+                      <div class="popup__close" data-close="true">
+                        <span class="popup__close_1"></span>
+                        <span class="popup__close_2"></span>
+                      </div>
+                      
+                      <div class="popup__content"></div> 
+                    </div>
                 </div>
                 `;
 
@@ -173,7 +215,6 @@
         }
 
         static defaultParams = {
-            cache: true,
             display: "block",
             data: {},
             paddingRightElements: [],
